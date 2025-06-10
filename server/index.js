5c7264200d248cab067eb5dda54122d96beb8bcd
bappidass <das184920@gmail.com>
2025-06-10 10:45:33 +0530

    chnage cors url

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: 'http://localhost:5173',
+    origin: 'https://realtime-task-manager-mern.onrender.com',
     methods: ['GET', 'POST', 'PATCH', 'DELETE'],
   },
 });
